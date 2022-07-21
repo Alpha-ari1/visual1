cd3cb264f693296a9f2e15891bcbcd4fb521e0a5
Alpha-ari1 <109708852+Alpha-ari1@users.noreply.github.com>
2022-07-20 22:49:45 -0400

    Add files via upload

---
+++ Streamlit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+
+
+# In[7]:
+
+
+st.title("Haciendo gráficos con altair en streamlit")
+
+df = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+
+st.write(df.head(5))
+
+c = alt.Chart(df).mark_circle().encode(
+     x='a', y='b', color='c', tooltip=['a', 'b', 'c'])
+
+st.altair_chart(c, use_container_width=True)
+
